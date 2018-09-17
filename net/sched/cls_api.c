ddcb76289e894110c2a4d23a70d2ab9659b9e43b
Chris Mi <chrism@mellanox.com>
2018-09-17 00:46:20 -0400

    net: sched: fix possible null pointer deref in tcf_block_put
    We need to check block for being null in both tcf_block_put and
    tcf_block_put_ext.
    
    Fixes: 343723dd51ef ("net: sched: fix clsact init error path")
    Reported-by: Prashant Bhole <bhole_prashant_q7@lab.ntt.co.jp>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -368,6 +368,8 @@ void tcf_block_put(struct tcf_block *block)
 {
 	struct tcf_block_ext_info ei = {0, };
 
+	if (!block)
+		return;
 	tcf_block_put_ext(block, block->q, &ei);
 }
 EXPORT_SYMBOL(tcf_block_put);
