a7d45d1db94d69c2931b6affd933ecf1332d33d5
Vlad Buslov <vladbu@nvidia.com>
2023-01-24 08:31:53 +0100

    debug
    Change-Id: Idd3046c259939e81f1b0eda501626df18cb37de1
    Signed-off-by: Vlad Buslov <vladbu@nvidia.com>

--- net/sched/act_ct.c
+++ net/sched/act_ct.c
@@ -251,6 +251,7 @@ static int tcf_ct_flow_table_fill_actions(struct net *net,
 		dir = IP_CT_DIR_ORIGINAL;
 		ctinfo = test_bit(IPS_SEEN_REPLY_BIT, &ct->status) ?
 			IP_CT_ESTABLISHED : IP_CT_NEW;
+		printk(KERN_WARNING"Setting ext_data to %x\n", ctinfo);
 		WRITE_ONCE(flow->ext_data, (void *)ctinfo);
 		break;
 	case FLOW_OFFLOAD_DIR_REPLY:
@@ -302,6 +303,7 @@ static bool tcf_ct_flow_table_get_timeout(struct nf_flowtable *ft,
 
 		*val = tn->timeouts[state];
 		*val -= tn->offload_timeout;
+		printk(KERN_WARNING"Timeout UDP %u\n", state);
 	} else {
 		return false;
 	}
@@ -679,6 +681,7 @@ static bool tcf_ct_flow_table_lookup(struct tcf_ct_params *p,
 			 * established state, then don't refresh.
 			 */
 			return false;
+		printk(KERN_WARNING"Reply dir packets. Bidir=%u, ctinfo=%x\n", test_bit(NF_FLOW_HW_BIDIRECTIONAL, &flow->flags), ctinfo);
 	}
 
 	if (tcph && (unlikely(tcph->fin || tcph->rst))) {
