b5de65e2c5b63b67f0b9856fe8119f43c318d726
Peilin Ye <peilin.ye@bytedance.com>
2023-05-23 00:13:20 -0700

    net/sched: sch_ingress: Only create under TC_H_INGRESS
    ingress Qdiscs are only supposed to be created under TC_H_INGRESS.
    Similar to mq_init(), return -EOPNOTSUPP if 'parent' is not
    TC_H_INGRESS.
    
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Reported-by: syzbot+b53a9c0d1ea4ad62da8b@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/r/0000000000006cf87705f79acf1a@google.com/
    Reviewed-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Acked-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: Peilin Ye <peilin.ye@bytedance.com>

--- net/sched/sch_ingress.c
+++ net/sched/sch_ingress.c
@@ -80,6 +80,9 @@ static int ingress_init(struct Qdisc *sch, struct nlattr *opt,
 	struct net_device *dev = qdisc_dev(sch);
 	int err;
 
+	if (sch->parent != TC_H_INGRESS)
+		return -EOPNOTSUPP;
+
 	net_inc_ingress_queue();
 
 	mini_qdisc_pair_init(&q->miniqp, sch, &dev->miniq_ingress);
