0e7bad9eedcb07478a378ede9b3255a92d786d0e
Vlad Buslov <vladbu@nvidia.com>
2023-08-03 15:06:08 +0200

    netfilter: flowtable: additional checks for outdated flows
    Current nf_flow_is_outdated() implementation considers any flow table flow
    which state diverged from its underlying CT connection status for teardown
    which can be problematic in the following cases:
    
    - Flow has never been offloaded to hardware in the first place either
    because flow table has hardware offload disabled (flag
    NF_FLOWTABLE_HW_OFFLOAD is not set) or because it is still pending on 'add'
    workqueue to be offloaded for the first time. The former is incorrect, the
    later generates excessive deletions and additions of flows.
    
    - Flow is already pending to be updated on the workqueue. Tearing down such
    flows will also generate excessive removals from the flow table, especially
    on highly loaded system where the latency to re-offload a flow via 'add'
    workqueue can be quite high.
    
    When considering a flow for teardown as outdated verify that it is both
    offloaded to hardware and doesn't have any pending updates.
    
    Issue: 3499413
    Fixes: 41f2c7c342d3 ("net/sched: act_ct: Fix promotion of offloaded unreplied tuple")
    Change-Id: Ic420e5f9eb857ddb848794a5286b39e74f655fbc
    Signed-off-by: Vlad Buslov <vladbu@nvidia.com>

--- net/netfilter/nf_flow_table_core.c
+++ net/netfilter/nf_flow_table_core.c
@@ -337,6 +337,8 @@ EXPORT_SYMBOL_GPL(flow_offload_refresh);
 static bool nf_flow_is_outdated(const struct flow_offload *flow)
 {
 	return test_bit(IPS_SEEN_REPLY_BIT, &flow->ct->status) &&
+		test_bit(IPS_HW_OFFLOAD_BIT, &flow->ct->status) &&
+		!test_bit(NF_FLOW_HW_PENDING, &flow->flags) &&
 		!test_bit(NF_FLOW_HW_ESTABLISHED, &flow->flags);
 }
 
