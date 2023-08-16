1f3d82669a99b0ac2719a8054cf634ddf19cdec7
Vlad Buslov <vladbu@nvidia.com>
2023-08-16 17:08:04 +0200

    Revert "net/sched: act_ct: Fix promotion of offloaded unreplied tuple"
    This reverts commit 41f2c7c342d3adb1c4dd5f2e3dd831adff16a669.

--- net/sched/act_ct.c
+++ net/sched/act_ct.c
@@ -610,7 +610,6 @@ static bool tcf_ct_flow_table_lookup(struct tcf_ct_params *p,
 	struct flow_offload_tuple tuple = {};
 	enum ip_conntrack_info ctinfo;
 	struct tcphdr *tcph = NULL;
-	bool force_refresh = false;
 	struct flow_offload *flow;
 	struct nf_conn *ct;
 	u8 dir;
@@ -648,7 +647,6 @@ static bool tcf_ct_flow_table_lookup(struct tcf_ct_params *p,
 			 * established state, then don't refresh.
 			 */
 			return false;
-		force_refresh = true;
 	}
 
 	if (tcph && (unlikely(tcph->fin || tcph->rst))) {
@@ -662,12 +660,7 @@ static bool tcf_ct_flow_table_lookup(struct tcf_ct_params *p,
 	else
 		ctinfo = IP_CT_ESTABLISHED_REPLY;
 
-	flow_offload_refresh(nf_ft, flow, force_refresh);
-	if (!test_bit(IPS_ASSURED_BIT, &ct->status)) {
-		/* Process this flow in SW to allow promoting to ASSURED */
-		return false;
-	}
-
+	flow_offload_refresh(nf_ft, flow);
 	nf_conntrack_get(&ct->ct_general);
 	nf_ct_set(skb, ct, ctinfo);
 	if (nf_ft->flags & NF_FLOWTABLE_COUNTER)
